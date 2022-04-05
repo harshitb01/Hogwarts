e8ef337d0cd67113723fa0269e24c03f3bad94e2
Harshit <harshitb2006@gmail.com>
2022-04-05 13:19:58 +0530

    database_connected

---
+++ Hogwarts_land/session.php
@@ -0,0 +1,8 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "harry");
+session_start();
+$user_check = $_SESSION['login_user'];
+$query = "SELECT username from register where username = '$user_check'";
+$ses_sql = mysqli_query($conn, $query);
+$row = mysqli_fetch_assoc($ses_sql);
+$login_session = $row['username'];

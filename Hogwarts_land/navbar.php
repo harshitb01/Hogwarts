c3ede5ded0643ae2bb28ec881b90b21c334a41dc
Harshit <harshitb2006@gmail.com>
2022-04-10 19:11:32 +0530

    none

--- Hogwarts_land/navbar.php
+++ Hogwarts_land/navbar.php
@@ -8,18 +8,13 @@ $db = mysqli_connect("localhost", "root", "", "harry");
 <head>
   <title>
   </title>
-
   <link rel="stylesheet" type="text/css" href="style.css">
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
   <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-
-
 </head>
 
 <body>
-
   <nav class="navbar navbar-inverse">
     <div class="container-fluid">
       <div class="navbar-header">
@@ -30,40 +25,23 @@ $db = mysqli_connect("localhost", "root", "", "harry");
         <li><a href="books.php">BOOKS</a></li>
         <li><a href="feedback.php">FEEDBACK</a></li>
       </ul>
-      <?php
-      if (isset($_SESSION['login_user'])) {
-
-      ?>
-        <ul class="nav navbar-nav">
-          <li><a href="user.php">PROFILE</a></li>
-          <li><a href="fine.php">FINES</a></li>
-
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
-          <li><a href="">
-              <div style="color: white">
-                <?php
-                $q = mysqli_query($db, "SELECT * FROM register where username='$_SESSION[login_user]' ;");
-                $row = mysqli_fetch_assoc($q);
-                echo "<img class='img-circle profile_img' height=30 width=30 src='Images/" . $row['user_image'] . "'>";
-                echo " " . $_SESSION['login_user'];
-                ?>
-              </div>
-            </a></li>
-          <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"> LOGOUT</span></a></li>
-        </ul>
-      <?php
-      } else {   ?>
-        <ul class="nav navbar-nav navbar-right">
-
-          <li><a href="student_login.php"><span class="glyphicon glyphicon-log-in"> LOGIN</span></a></li>
-
-          <li><a href="registration.php"><span class="glyphicon glyphicon-user"> SIGN UP</span></a></li>
-        </ul>
-      <?php
-      }
-      ?>
-
+      <ul class="nav navbar-nav">
+        <li><a href="user.php">PROFILE</a></li>
+        <li><a href="fine.php">FINES</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="">
+            <div style="color: white">
+              <?php
+              $q = mysqli_query($db, "SELECT * FROM register where username='$_SESSION[login_user]' ;");
+              $row = mysqli_fetch_assoc($q);
+              echo "<img class='img-circle profile_img' height=30 width=30 src='Images/" . $row['user_image'] . "'>";
+              echo " " . $_SESSION['login_user'];
+              ?>
+            </div>
+          </a></li>
+        <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"> LOGOUT</span></a></li>
+      </ul>
     </div>
   </nav>
 </body>
--- Hogwarts_land/php/header.php
+++ Hogwarts_land/php/header.php
@@ -1,3 +1,8 @@
+<?php
+$db = mysqli_connect("localhost", "root", "", "harry");
+?>
+
+
 <header id="header">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a href="profile.php" class="navbar-brand">
@@ -6,12 +11,25 @@
             </h3>
         </a>
         <ul id="navbar">
-                <!-- <div style="float:left" class="navHeading">Hogwarts Hub</div> -->
-                <li><a href="">LOGIN</a></li>
-                <li><a href="">DIAGON ALLEY</a></li>
-                <!-- <li><a href="table.html">MEET OUR STAR CAST</a></li> -->
-                <li><a href="about.html">ABOUT</a></li>
-                <li><a href="" class="btnLogout glyphicon-log-out" style="float:right">LOGOUT</a></li>
+            <!-- <div style="float:left" class="navHeading">Hogwarts Hub</div> -->
+            <li><a href="">LOGIN</a></li>
+            <li><a href="">DIAGON ALLEY</a></li>
+            <!-- <li><a href="table.html">MEET OUR STAR CAST</a></li> -->
+            <li><a href="about.html">ABOUT</a></li>
+            <!-- <li><a href="" class="btnLogout glyphicon-log-out" style="float:right">LOGOUT</a></li> -->
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="">
+                    <div style="color: white">
+                        <?php
+                        $q = mysqli_query($db, "SELECT * FROM register where username='$_SESSION[login_user]' ;");
+                        $row = mysqli_fetch_assoc($q);
+                        echo "<img class='img-circle profile_img' height=30 width=30 src='Images/" . $row['user_image'] . "'>";
+                        echo " " . $_SESSION['login_user'];
+                        ?>
+                    </div>
+                </a></li>
+            <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"> LOGOUT</span></a></li>
         </ul>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>

59735d1e8b2b3a038abe317f4ce2e2897f5798e7
aryaharshit <arya.harshit1964@gmail.com>
2022-04-10 20:33:02 +0530

    footer

--- Hogwarts_land/cart.php
+++ Hogwarts_land/cart.php
@@ -30,6 +30,9 @@ if (isset($_POST['remove'])) {
     <!-- Bootstrap CDN -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="style-cart.css">
+    <link rel="stylesheet" href="style-cart-foot.css" />
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 
 <body class="bg-light">
@@ -102,6 +105,31 @@ if (isset($_POST['remove'])) {
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="jQuery.js"></script>
+    <footer class="main-footer">
+        <div class="container main-footer-container">
+            <h3 class="band-name">The Gryffindor's Hub</h3>
+            <div class="icon facebook">
+                <div class="tooltip">Facebook</div>
+                <span><a href="https://www.facebook.com/wizardingworld/"><i class="fab fa-facebook-f"></i></a></span>
+            </div>
+
+            <div class="icon twitter">
+                <div class="tooltip">Twitter</div>
+                <span><a href="https://twitter.com/wizardingworld?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"><i class="fab fa-twitter"></i></a></span>
+            </div>
+
+            <div class="icon instagram">
+                <div class="tooltip">Instagram</div>
+                <span><a href="https://www.instagram.com/wizardingworld/?hl=en"><i class="fab fa-instagram"></i></a></span>
+            </div>
+
+            <div class="icon youtube">
+                <div class="tooltip">YouTube</div>
+                <span><a href="https://www.youtube.com/c/WizardingWorld/videos"><i class="fab fa-youtube"></i></a></span>
+            </div>
+        </div>
+    </footer>
 </body>
 
 </html>
